d12557ca814b55111fe1bc44c45baa67bfea0b7e
achmadhanafy <achmadhanafy@gmail.com>
2022-12-20 11:17:15 +0700

    Conneting to mongoDb Atlas

--- server/app.js
+++ server/app.js
@@ -9,12 +9,22 @@ const app = express();
 // allow cross-origin requests
 app.use(cors());
 
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+
+mongoose.connect('mongodb+srv://root:Password09!@cluster0.ixqay.mongodb.net/?retryWrites=true&w=majority',connectionParams)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
-mongoose.connection.once('open', () => {
-    console.log('conneted to database');
-});
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
